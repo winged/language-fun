fea0a592c4d48c09578fde8b4554ae4269462bd7
David Vogt <dv@winged.ch>
2012-07-06 22:28:42 +0200

    Refactored parser (backwards compatible):
    The Parser is now split into a tokenizer and a decoder.
    
    How it works is this: The input is fed to the tokenizer, which
    returns an array of tokens. A token may be an operator, or
    an operand. The tokenizer already filters out comments and empty
    lines as well as anything else undesirable. If something unknown
    appears, the tokenizer logs an error.
    
    Then, the parser reads the first token - which must be an operator.
    It then tries to find that operator in the opcodeInfo table. If found,
    it fetches the required number of operands from the token list, and
    feeds it to the decoder.
    
    The decoder in turn modifies the opcode to reflect the (possible)
    indirection of the parameter. It then returns a list containing
    the final opcode (including variant) and a decoded list of it's
    operands.
    
    The parser then adds those to the memory image, and continues with
    the next operator, until it runs out of tokens.
    
    I think that this design, while a bit harder to understand, will be
    better in the future when we want to add syntactical extensions, such
    as aliases, and macros.

--- DVM02/parser.js
+++ DVM02/parser.js
@@ -2,111 +2,164 @@
 exports.PARSER = function(log) {
 	var opcodeInfo = {
 		// MSB defines opcode base, LSB defines variant
-		'START':  {base: 0x00, needlength: 1},
-		'NOP':    {base: 0x00, needlength: 1},
-		'READC':  {base: 0x10, needlength: 2},
-		'WRITEC': {base: 0x20, needlength: 2},
-		'WRITEI': {base: 0x30, needlength: 2},
-		'SET':    {base: 0x40, needlength: 3},
-		'ADD':    {base: 0x50, needlength: 3},
-		'SUB':    {base: 0x60, needlength: 3},
-		'MUL':    {base: 0x70, needlength: 3},
-		'DIV':    {base: 0x80, needlength: 3},
-		'JZ':     {base: 0x90, needlength: 3},
-		'JNZ':    {base: 0xA0, needlength: 3},
-		'JGT':    {base: 0xB0, needlength: 4},
-		'COPY':   {base: 0xC0, needlength: 3},
-		'DUMP':   {base: 0xE0, needlength: 3},
+		'START':  {base: 0x00, numArgs: 0},
+		'NOP':    {base: 0x00, numArgs: 0},
+		'READC':  {base: 0x10, numArgs: 1},
+		'WRITEC': {base: 0x20, numArgs: 1},
+		'WRITEI': {base: 0x30, numArgs: 1},
+		'SET':    {base: 0x40, numArgs: 2},
+		'ADD':    {base: 0x50, numArgs: 2},
+		'SUB':    {base: 0x60, numArgs: 2},
+		'MUL':    {base: 0x70, numArgs: 2},
+		'DIV':    {base: 0x80, numArgs: 2},
+		'JZ':     {base: 0x90, numArgs: 2},
+		'JNZ':    {base: 0xA0, numArgs: 2},
+		'JGT':    {base: 0xB0, numArgs: 3},
+		'COPY':   {base: 0xC0, numArgs: 2},
+		'DUMP':   {base: 0xE0, numArgs: 2},
 		// ...
-		'END':    {base: 0xF0, needlength: 1},
+		'END':    {base: 0xF0, numArgs: 0},
 	}
 
-	function parseLine(line, lineno) {
-		var chomped = line
-			.replace(/#.*/, '') // remove comments
-			.replace(/^\s*/, '') // remove leading space
-			.replace(/\s*$/, '') // remove trailing space
-		;
-
-		if(chomped.length == 0) {
-			// empty line, or (already-removed) comment
-			return [];
-		}
-		var words = chomped.split(/\s+/);
-
-
-		log("PARSER: LINE " + lineno + ": " + chomped);
-
-		// see if opcode is valid
-		if(words[0] in opcodeInfo) {
-			var op_text = words[0];
-			var opinfo = opcodeInfo[op_text];
-			// convert opcode text to integer
-			words[0] = opinfo.base;
-			
-			// check operation length
-			if (opinfo.needlength != words.length) {
-				var error = "Syntax error at line " + lineno + ": " +
-					"Opcode " + op_text + " requires " + (opinfo.needlength-1) +
-					" parameters, got " + (words.length-1);
-				log("PARSER: " + error);
-				return error;
-			}
-
-		}
-		else {
-			var error = "Syntax error at line " + lineno + ": " + "Opcode not known: " + words[0];
-			log("PARSER: " + error);
-			return error;
-		}
-
-		// parse parameters
-		var variant = 0;
-		for (var i = 1; i < words.length; i++) {
-			var word = words[i];
-			if (word[0] == '@') {
-				// indirect param. set variant for that position
-				var pos = i - 1;
-				log("        Param "+i+" is indirect");
-				var mask = 1 << pos;
-				variant = variant | mask;
-				words[i] = parseInt(word.substr(1));
-			}
-			else {
-				words[i] = parseInt(word);
-			}
-		}
-
-		words[0] = words[0] + variant;
-		log("        OP combined: "+words[0].toString(16));
-		
-		return words;
-	}
 
 	var okay  = true;
 	var error = '';
+	function decode(opBase, operands) {
+		// Last 4 bits of opBase are markers for indirect
+		// access to the operand. They will be set to 1
+		// if the operand starts with an @
+
+		operation = [];
+		for(i=0; i < operands.length; i++) {
+			var operand = operands[i];
+			if (operand.content[0] == '@') {
+				// Indirect! Depending on which operand we're working on,
+				// we need to set the corresponding bit:
+				//       i = 0  -->  0b0001
+				//       i = 1  -->  0b0010
+				//       i = 2  -->  0b0100
+				//       i = 3  -->  0b1000
+				// then the last bit should be set to 1, resulting in 1101 0001
+				opBase = opBase | (1 << i);
+
+				// Remove the @ from the operand
+				operand.content = operand.content.substr(1);
+			}
+
+			// TODO: decode aliases
+			operation.push(parseInt(operand.content));
+		}
+
+		// Before returning, we need to insert the operator
+		// in front of our list. Not very efficient, but simple.
+		operation.unshift(opBase);
+		return operation;
+	}
 
 	function parse(text) {
-		okay  = true;
-		error = '';
+		var tokens = tokenize(text);
 
-		var lines = text.split(/\n/);
+		if(tokens == null) {
+			// Something bad happened
+			return null;
+		}
 
-		var ll = lines.length;
-		memory = [];
-		for (var i = 0; i < ll; i++) {
-			var operation = parseLine(lines[i], i+1);
-			if (typeof operation == 'string') {
-				error = operation;
-				okay  = false;
-				return false;
+		var memory = [];
+		var opcode;
+		while (opcode = tokens.shift()) {
+			// opcode.content MUST be an operation - otherwise
+			// we have a syntax error.
+			if(!(opcode.content in opcodeInfo)) {
+				log("Syntax error on line " + opcode.line + ": Opcode unknown:" + opcode.content)
+				return null;
 			}
+			var info = opcodeInfo[opcode.content];
+
+			var operands = []
+			for (var i = 0; i < info.numArgs; i++) {
+				operands.push(tokens.shift());
+			}
+
+			// Decode operation: Operands may still contain
+			// indirections(@). Note that
+			// we already pass the integer form of the opcode
+			// to the decode function - the name is not required
+			// anymore...
+
+			var operation = decode(info.base, operands);
+
+			// Store decoded operation in memory
 			for (var j = 0; j < operation.length; j++) {
 				memory.push(operation[j]);
 			}
 		}
 
+		if (memory == []) {
+			// No code, or other problem
+			return null;
+		}
+
 		return memory;
+	}
+
+	function tokenize(text) {
+
+		tokens = [];
+
+		var currentLine = 1;
+
+		function word(match) {
+			tokens.push({
+				line:    currentLine,
+				content: match[1]
+			});
+		}
+		function dontCare(match) {
+			// Not interested, not doing anything
+		}
+		function newline(match) {
+			currentLine++;
+		}
+
+		// Regexes used for parsing. The parser works as
+		// follows: Each of those regexps is tried on the
+		// input. If one matches, the corresponding callback
+		// function is called, and the matching part is
+		// removed from the input. This is then repeated until
+		// either the string didn't change it's size (which means
+		// something unknown in input), or the input string is
+		// reduced to an empty string (parsing done).
+		var checks = [
+			{ re: /^([!@\w\d]+)/, cb: word      }, // operator, operand
+			{ re: /^#[^\n]*/,     cb: dontCare }, // a comment
+			{ re: /^[\n]/,        cb: newline   }, // newline
+			{ re: /^[\s]+/,       cb: dontCare }  // other whitespace
+		]
+
+		while (text.length > 0) {
+			var lenBefore = text.length;
+			for (var i = 0; i < checks.length; i++) {
+				var check = checks[i];
+				var match = text.match(check.re);
+				if (match == null) {
+					continue;
+				}
+				// If we reach this line, we have a match!
+				check.cb(match);
+				text = text.replace(check.re, '');
+			}
+			var lenAfter = text.length;
+			if (lenAfter == lenBefore) {
+				// Oops, none of our matches worked! This means syntax error
+				nextFewBytes = text.substr(0, 10);
+				log("Error: Unknown input near ..." + nextFewBytes + "... on line " + currentLine);
+
+				// Return null, so caller knows something went wrong
+				return null;
+			}
+		}
+
+		return tokens;
 	}
 
 	return {
